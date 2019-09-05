4d72666d5bbdf086cf7c57ac264cbc640d536b9b
gex713 <gex713@yahoo.com>
2019-09-05 15:22:47 -0400

    Got the bird to flap and brackground to show

--- music-city-game-1.1/MyGame/game.js
+++ music-city-game-1.1/MyGame/game.js
@@ -1,5 +1,20 @@
+const START_X = 0
+const START_Y = 540
+
+class Entity {
+    constructor(x, y, size) {
+        Object.assign(this, {x, y, size})
+    }
+}
+
+class Flappy extends Entity {
+    constructor(x, y) {
+        super(x, y, 55)
+    }
+}
+
 export default class Game {
     constructor() {
-        console.log('Game: hello, world!')
+        this.flappy = new Flappy(START_X, START_Y)
     }
 }
--- music-city-game-1.1/MyGame/view.js
+++ music-city-game-1.1/MyGame/view.js
@@ -1,5 +1,30 @@
+function Sprite(path, n = 1) {
+    const files = Array(n).fill(true).map((_, n) => `${path}${n}.png`)
+    return files.map(f => {
+        const im = new Image()
+        im.src = f
+        return im
+    })
+}
+
 export default class View {
     constructor() {
-        console.log('View: hello, world!')
+        this.bg = Sprite('assets/background')
+        this.flappy = Sprite('assets/flappy', 4)
+    }
+
+    render(game, ctx) {
+        const flappyFrame = Math.floor(performance.now() / 100) % this.flappy.length
+        const flappy = this.flappy[flappyFrame]
+        const flappyX = game.flappy.x - flappy.width * 0.6
+        const flappyY = game.flappy.y - flappy.height * 0.5
+
+        ctx.save()
+            ctx.drawImage(this.bg[0], 0, 0)
+            ctx.save()
+                ctx.translate(250 - game.flappy.x, 0)
+                ctx.drawImage(flappy, flappyX, flappyY)
+            ctx.restore()
+        ctx.restore()
     }
 }
